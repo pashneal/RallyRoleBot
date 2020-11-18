ccfdcb90369d9c95130125700c4c1929edaf7b66
Ashneal Powell <pashneal@gmail.com>
2020-11-18 11:44:35 -0500

    Clean up Role Cog by removing extraneous checks, using discord Converters

---
+++ rallyrolebot/role_cog.py
@@ -0,0 +1,86 @@
+import json
+import sys
+import traceback
+
+import discord
+from discord.ext import commands, tasks
+from discord.utils import get
+
+import data
+import rally_api
+import update_cog
+import validation
+import errors
+
+from constants import *
+from utils import pretty_print
+
+
+class RoleCommands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+        
+    async def cog_after_invoke(self, ctx):
+        await pretty_print( ctx, "Command completed successfully!",  title= "Success", color=SUCCESS_COLOR)
+
+    @errors.standard_error_handler
+    async def cog_command_error(self, ctx, error):
+        # All other Errors not returned come here. And we can just print the default TraceBack.
+        print(
+            "Ignoring exception in command {}:".format(ctx.command), file=sys.stderr
+        )
+        traceback.print_exception(
+            type(error), error, error.__traceback__, file=sys.stderr
+        )
+
+    @commands.command(
+        name="set_role_mapping",
+        help=" <coin name> <coin amount> <role name> "
+        + "Set a mapping between coin and role. Roles will be constantly updated.",
+    )
+    @validation.owner_or_permissions(administrator=True)
+    async def set_coin_for_role(self, ctx, coin_name, coin_amount: int, role : discord.Role):
+        data.add_role_coin_mapping(ctx.guild.id, coin_name, coin_amount, role.name)
+
+    @commands.command(
+        name="one_time_role_mapping",
+        help=" <coin name> <coin amount> <role name>"
+        + " Set a mapping to be applied once instantly.",
+    )
+    @validation.owner_or_permissions(administrator=True)
+    async def one_time_role_mapping(self, ctx, coin_name, coin_amount: int, role : discord.Role ):
+        for member in ctx.guild.members:
+            rally_id = data.get_rally_id(member.id)
+            if rally_id:
+                balances = rally_api.get_balances(rally_id)
+                await update_cog.grant_deny_role_to_member(
+                    {
+                        data.GUILD_ID_KEY: ctx.guild.id,
+                        data.COIN_KIND_KEY: coin_name,
+                        data.REQUIRED_BALANCE_KEY: coin_amount,
+                        data.ROLE_NAME_KEY: role.name,
+                    },
+                    member,
+                    balances,
+                )
+
+    @commands.command(
+        name="unset_role_mapping",
+        help=" <coin name> <coin amount> <role name> "
+        + "Unset a mapping between coin and role",
+    )
+    @validation.owner_or_permissions(administrator=True)
+    async def unset_coin_for_role(self, ctx, coin_name, coin_amount: int, role : discord.Role):
+        data.remove_role_mapping(ctx.guild.id, coin_name, coin_amount, role.name)
+
+    @commands.command(name="get_role_mappings", help="Get role mappings")
+    @validation.owner_or_permissions(administrator=True)
+    async def get_role_mappings(self, ctx):
+        await ctx.send(
+            json.dumps(
+                [
+                    json.dumps(mapping)
+                    for mapping in data.get_role_mappings(ctx.guild.id)
+                ]
+            )
+        )
